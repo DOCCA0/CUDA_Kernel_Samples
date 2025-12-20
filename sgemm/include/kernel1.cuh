b5a732b2e0e3b846518d07b2da802b19078acc12
DOCCAO <2947323341@qq.com>
2025-12-21 00:11:24 +0100

    1

--- sgemm/include/kernel1.cuh
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-__global__ __launch_bounds__(1024) 
-void sgemm_v1(int M, int N, int K, float alpha, float *A, float *B, float beta, float *C);
--- sgemm/include/kernel2.cuh
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-template<const int BLOCK_SIZE>
-__global__ void sgemm_v2(int M, int N, int K, float alpha, float *A, float *B, float beta, float *C);
