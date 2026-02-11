57c54115d974559e5ade3e5a0b064f77a77d6b8d
DOCCA0 <2947323341@qq.com>
2026-02-11 16:48:46 +0100

    sgemm

--- gemm/sgemm.cu
+++ gemm/sgemm.cu
@@ -303,7 +303,7 @@ __global__ void sgemm_v6( float *A,  float *B, float *C, int M, int N, int K, fl
     // (row_a,col_a) 是线程负责的 A 矩阵块内的起始位置
     int col_a =(threadIdx.x % (BK/4)) * 4;
     int row_a = threadIdx.x / (BK/4);
-    // stride 是需要跳过的行数
+    // stride 是需要跳过的行数(转置后为列)
     int stride_a = thread_num/(BK/4);
 
     // (row_b,col_b) 是线程负责的 B 矩阵块内的起始位置
@@ -334,8 +334,8 @@ __global__ void sgemm_v6( float *A,  float *B, float *C, int M, int N, int K, fl
         }
         // Bs上行向移动
         #pragma unroll
-        for (int i=0;i<BN; i += stride_b) {
-            FETCH_FLOAT4(Bs[(row_b + i)*BN][col_b]) = FETCH_FLOAT4(B[(row_b + i) * N + col_b]); // 不需要转置
+        for (int i=0;i<BK; i += stride_b) {
+            FETCH_FLOAT4(Bs[row_b + i][col_b]) = FETCH_FLOAT4(B[(row_b + i) * N + col_b]); // 不需要转置
         }
         __syncthreads();
         #pragma unroll
@@ -343,7 +343,7 @@ __global__ void sgemm_v6( float *A,  float *B, float *C, int M, int N, int K, fl
             #pragma unroll
             for (int m=0;m<TM;m+=4){
                 // 转置就是为了这里
-                FETCH_FLOAT4(a_reg[m]) = FETCH_FLOAT4(As[i][(row_c + m) ]);
+                FETCH_FLOAT4(a_reg[m]) = FETCH_FLOAT4(As[i * BM][(row_c + m) ]);
             }
             #pragma unroll
             for (int n=0;n<TN;n+=4){
@@ -364,6 +364,160 @@ __global__ void sgemm_v6( float *A,  float *B, float *C, int M, int N, int K, fl
 
     #pragma unroll
     for (int m=0;m<TM;++m){
+        for (int n =0;n<TN;++n){ 
+            float4  c_fl4 =FETCH_FLOAT4(C[(row_c + m) * N + col_c + n]);
+            c_fl4.x = alpha * tmp[m][n] + beta * c_fl4.x;
+            c_fl4.y = alpha * tmp[m][n] + beta * c_fl4.y;
+            c_fl4.z = alpha * tmp[m][n] + beta * c_fl4.z;
+            c_fl4.w = alpha * tmp[m][n] + beta * c_fl4.w;
+            FETCH_FLOAT4(C[(row_c + m) * N + col_c + n]) = c_fl4;
+        }
+    }
+}
+
+
+// v7 预取下一轮数据到寄存器，计算和访存重叠
+#define FETCH_FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
+template <const int BM , const int BN, const int BK, const int TM, const int TN>
+__global__ void sgemm_v7( float *A,  float *B, float *C, int M, int N, int K, float alpha, float beta) {
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+
+    int thread_num = BM * BN / (TM * TN);
+
+    // 一个线程负责计算 TM * TN 个 C 矩阵元素
+    // 一共需要 BM/TM * BN/TN 个线程
+    // (row_c, col_c) 是 C 矩阵块内的左上角起始位置
+    int col_c = (threadIdx.x % (BN/TN)) * TN;
+    int row_c = (threadIdx.x / (BN/TN)) * TM ;
+
+    // As 转置，为了float4读取列
+    // 增加一倍用于缓存
+    __shared__ float As[2][BK][BM];
+    __shared__ float Bs[2][BK][BN];
+
+    A = &A[by * BM * K];
+    B = &B[bx * BN];
+    C = &C[by * BM * N + bx * BN];
+
+    // (row_a,col_a) 是线程负责的 A 矩阵块内的起始位置
+    int col_a =(threadIdx.x % (BK/4)) * 4;
+    int row_a = threadIdx.x / (BK/4);
+    // stride 是需要跳过的行数(转置后为列)
+    int stride_a = thread_num/(BK/4);
+
+    // (row_b,col_b) 是线程负责的 B 矩阵块内的起始位置
+    int col_b = (threadIdx.x % (BN/4)) * 4;
+    int row_b = threadIdx.x / (BN/4);
+    int stride_b = thread_num / (BN/4);
+
+    // 每个线程负责TM*TN个元素，则需要申请TM*TN个寄存器保存累加值
+    float tmp[TM][TN]= {0.f};
+    float ldg_a_reg[BK * BM / thread_num] = {0.}; // 每个线程负责多少个As的搬运
+    float ldg_b_reg[BK * BN / thread_num] = {0.}; // 每个线程负责多少个Bs的搬运
+
+    float a_reg[2][TM];
+    float b_reg[2][TN];
+
+    // 预取第一轮数据
+    #pragma unroll
+    for (int i=0;i<BM; i += stride_a) {
+        // 0 4 8 12 ... 
+        int ldg_index = i / stride_a * 4;  // 第ldg_index轮
+        FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(A[(row_a + i) * K + col_a]);
+        // As转置存，fl4中间缓存，减少共享内存访问冲突
+        As[0][(col_a)][ i + row_a] = ldg_a_reg[ldg_index];
+        As[0][(col_a + 1)][ i + row_a] = ldg_a_reg[ldg_index+1];
+        As[0][(col_a + 2)][ i + row_a] = ldg_a_reg[ldg_index+2];
+        As[0][(col_a + 3)][ i + row_a] = ldg_a_reg[ldg_index+3];
+    }
+    #pragma unroll
+    for (int i=0;i<BK; i += stride_b) {
+        FETCH_FLOAT4(Bs[0][row_b + i][col_b]) = FETCH_FLOAT4(B[(row_b + i) * N + col_b]); // 不需要转置
+    }
+    int write_index = 1;
+    int load_index;
+    int k=0;
+    do {
+        __syncthreads();
+        k+=BK;
+        if(k<K){
+            #pragma unroll
+            for (int i=0;i<BM; i += stride_a) {
+                int ldg_index = i / stride_a * 4;  // 第ldg_index轮
+                FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(A[(row_a + i) * K + col_a + k]);
+            }
+            #pragma unroll
+            for (int i=0;i<BK; i += stride_b) {
+                int ldg_index = i / stride_b * 4;  // 第ldg_index轮
+                FETCH_FLOAT4(ldg_b_reg[ldg_index]) = FETCH_FLOAT4(B[(row_b + i) * N + col_b + k]);
+            }
+        }
+        load_index = write_index^1;
+        #pragma unroll
+        for (int m=0;m<TM;m+=4){
+            // 转置就是为了这里
+            FETCH_FLOAT4(a_reg[0][m]) = FETCH_FLOAT4(As[load_index][0 * BM][(row_c + m) ]);
+        }
+        #pragma unroll
+        for (int n=0;n<TN;n+=4){
+            FETCH_FLOAT4(b_reg[0][n]) = FETCH_FLOAT4(Bs[load_index][0 * BN][col_c + n]);
+        }
+
+        // 加载下一次迭代的数据
+        #pragma unroll
+        for(int bk=0;bk<BK-1;++bk){
+            for (int m=0;m<TM;m+=4){
+                // 转置就是为了这里
+                FETCH_FLOAT4(a_reg[(bk+1)%2][m]) = FETCH_FLOAT4(As[load_index][(bk+1) * BM][(row_c + m) ]);
+            }
+            #pragma unroll
+            for (int n=0;n<TN;n+=4){
+                FETCH_FLOAT4(b_reg[(bk+1)%2][n]) = FETCH_FLOAT4(Bs[load_index][(bk+1) * BN][col_c + n]);
+            }
+            #pragma unroll
+            for (int m=0;m<TM;++m){
+                for (int n=0;n<TN;++n){
+                    tmp[m][n] += a_reg[bk%2][m] * b_reg[bk%2][n];
+                }
+            }
+        }
+        // 前面只算了BK-1轮，最后一轮在这里算
+        #pragma unroll
+        for (int m=0;m<TM;++m){
+            for (int n=0;n<TN;++n){
+                tmp[m][n] += a_reg[(BK-1)%2][m] * b_reg[(BK-1)%2][n];
+            }
+        }
+
+        // __syncthreads();  // 这里不需要同步了，因为上面的As(Bs)[load_index]和下面的As(Bs)[write_index]的内存是分开的
+        if (k < K) {
+            #pragma unroll
+            for (int i=0;i<BM; i += stride_a) {
+                // 0 4 8 12 ... 
+                int ldg_index = i / stride_a * 4;  // 第ldg_index轮
+                // As转置存，fl4中间缓存，减少共享内存访问冲突
+                As[0][(col_a)][ i + row_a] = ldg_a_reg[ldg_index];
+                As[0][(col_a + 1)][ i + row_a] = ldg_a_reg[ldg_index+1];
+                As[0][(col_a + 2)][ i + row_a] = ldg_a_reg[ldg_index+2];
+                As[0][(col_a + 3)][ i + row_a] = ldg_a_reg[ldg_index+3];
+            }
+            #pragma unroll
+            for (int i=0;i<BK; i += stride_b) {
+                // 0 4 8 12 ... 
+                int ldg_index = i / stride_b * 4;  // 第ldg_index轮
+                FETCH_FLOAT4(Bs[write_index][row_b + i][col_b]) = FETCH_FLOAT4(B[(row_b + i) * N + col_b]); // 不需要转置
+            }
+        }
+        write_index = load_index^1;
+    }while (k < K);
+
+
+    // C=alpha*A*B + beta*C
+    #pragma unroll
+    for (int m=0;m<TM;++m){
+        #pragma unroll
         for (int n =0;n<TN;++n){ 
             float4  c_fl4 =FETCH_FLOAT4(C[(row_c + m) * N + col_c + n]);
             c_fl4.x = alpha * tmp[m][n] + beta * c_fl4.x;
